0d1a080927970ae9c34c9a16516c672131724ae0
Obeida Iqbal <obeidaiqbal41@gmail.com>
2025-07-23 16:30:10 -0700

    Added camera library access functionality for reading text from photos

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,15 +1,38 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { Link } from 'expo-router'; 
+import { Image } from 'expo-image';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
 
+
+const PlaceholderImage = require('@/assets/images/icon.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+      </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Button A" />
-        <Button label="Button B" />
+        <Button theme="primary" label="Button A" onPress={pickImageAsync} />
+        <Button label="Button B" onPress={() => alert('You pressed a button.')} />
       </View>
     </View>
   );
@@ -29,6 +52,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#000000ff',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
   footerContainer: {
     position: 'absolute',
